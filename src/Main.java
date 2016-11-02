a70a0e6d92a0be658a6366a4ce57022154bcbb55
Aline Brito <nbrito.aline@gmail.com>
2016-11-02 16:39:59 -0200

    Tempo de execução e consumo de memória escritos no arquivo de saída.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,26 +17,39 @@ public class Main {
 			String option = sc.nextLine();
 			long timeInit = 0;
 			long timeFinal = 0;
+			long totalMemory = 0;
+			long freeMemory = 0;
+			long memory = 0;
+			long time = 0;
+			List<String> result = new ArrayList<String>();
 			if(option.equals("1")){
 				System.out.println("Informe o arquivo de entrada: \n>>");
 				String nameFile = sc.nextLine();
 				File fileIn = new File(nameFile); //Arquivo de entrada.
 				System.out.println("Calculando o Baseline (Aguarde)... ");
-				timeInit = System.currentTimeMillis();
 				Graph g = new Graph(fileIn);
-				List<String> result = g.calcBaseline();
+				timeInit = System.currentTimeMillis();
+				result = g.calcBaseline();
 				timeFinal = System.currentTimeMillis();
-				UtilFile.writeFile("output-baseline.txt", result);
 				System.out.println("Cálculo finalizado, verifique o resultado no arquivo <output-baseline.txt>");
 			}
 			else {
 				System.err.println("Opção Inválida!\n");
 			}
 			
-			System.out.println("Tempo de Execução: " + (timeFinal - timeInit) + "ms.");
+			time = (timeFinal - timeInit);
+			totalMemory = Runtime.getRuntime().totalMemory();
+			freeMemory = Runtime.getRuntime().freeMemory();
+			memory = (totalMemory - freeMemory);
+			System.out.println("Tempo de Execução: " + time + "ms.");
+			System.out.println("Memória usada: " + memory + " bytes.");
+			result.add("Memória usada: " + memory + " bytes.");
+			result.add("Tempo de Execução: " + time + "ms.");
+			UtilFile.writeFile("output.txt", result);
 			
 		} catch (Exception e) {
 			System.err.println("\n[ERROR]: Falha ao calcular. Verifique o formato do arquivo de entrada utilizado.\n");
+			e.printStackTrace();
 		}
 	}
 
